bd90daeb7eea7b0ec2bc0314a1ac3615ffbd4fc1
atg-abhijay <atg.abhijay@gmail.com>
2018-08-28 16:57:23 -0400

    solved 771: jewels and stones

---
+++ jewels_stones_771.py
@@ -0,0 +1,16 @@
+"""
+URL of problem: https://leetcode.com/problems/jewels-and-stones/description/
+"""
+
+def main(J, S):
+    num_jewels = 0
+    jewels_set = set(J)
+    # print(jewels_set)
+    for stone in S:
+        if stone in jewels_set:
+            num_jewels += 1
+
+    # print(num_jewels)
+    return num_jewels
+
+main(input("Give string of jewels: "), input("Give string of stones: "))

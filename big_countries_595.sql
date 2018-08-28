aa1835f6146d5b7b7cf81b18cdc2bb098be70dfb
atg-abhijay <atg.abhijay@gmail.com>
2018-08-28 17:21:00 -0400

    solved 595: Big Countries

---
+++ big_countries_595.sql
@@ -0,0 +1,9 @@
+/*
+URL of problem: https://leetcode.com/problems/big-countries/description/
+*/
+
+Select name, population, area
+From World
+Where
+    area > 3000000
+    or population > 25000000

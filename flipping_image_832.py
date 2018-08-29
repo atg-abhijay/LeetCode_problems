08bb97368f432ff08900218f4bfe975ad079da87
atg-abhijay <atg.abhijay@gmail.com>
2018-08-28 21:26:53 -0400

    solved 832: Flipping An Image - added comments as well

--- flipping_image_832.py
+++ flipping_image_832.py
@@ -3,6 +3,18 @@ URL of problem: https://leetcode.com/problems/flipping-an-image/description/
 """
 
 def main():
-    pass
+    A = [[1,1,0],[1,0,1],[0,0,0]]
+    B = []
+    for row in A:
+        r = reversed(row)
+        p = []
+        for num in r:
+            # (1-num) gives us the
+            # complement for 0s and 1s
+            p.append(1-num)
+        B.append(p)
+
+    # print(B)
+    return B
 
 main()

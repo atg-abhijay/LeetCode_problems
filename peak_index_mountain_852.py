23d3f2b884a6355e9561ce1e423a4d58976998c9
atg-abhijay <atg.abhijay@gmail.com>
2018-08-29 19:27:14 -0400

    solved 852: Peak Index in a Mountain Array

--- peak_index_mountain_852.py
+++ peak_index_mountain_852.py
@@ -2,8 +2,18 @@
 URL of problem: https://leetcode.com/problems/peak-index-in-a-mountain-array/description/
 """
 
-def main():
-    pass
+def main(A):
+    max_num = 0
+    max_index = 0
+    obj = enumerate(A)
+    for index, num in obj:
+        if num > max_num:
+            max_num = num
+            max_index = index
+
+    print("Index with max value:", max_index)
+    # return max_index
+
 
 if __name__ == '__main__':
-    main()
+    main([0, 2, 1, 0])

af141f4c37ed67fada0cb25631eefa986cd0a037
atg-abhijay <atg.abhijay@gmail.com>
2018-08-29 19:02:53 -0400

    solved 657: Judge Route Circle and added comments

--- judge_route_circle_657.py
+++ judge_route_circle_657.py
@@ -2,8 +2,29 @@
 URL of problem: https://leetcode.com/problems/judge-route-circle/description/
 """
 
-def main():
-    pass
+def main(moves):
+    moves_list = list(moves)
+    x = 0
+    y = 0
+    for move in moves_list:
+        if move == 'U':
+            y += 1
+        elif move == 'D':
+            y -= 1
+        elif move == 'R':
+            x += 1
+        elif move == 'L':
+            x -= 1
+
+    # if both x and y are 0,
+    # then we are at the origin
+    if x == 0 and y == 0:
+        # print("True")
+        return True
+
+    # print("False")
+    return False
+
 
 if __name__ == '__main__':
-    main()
+    main(input("Give input string for moves: "))

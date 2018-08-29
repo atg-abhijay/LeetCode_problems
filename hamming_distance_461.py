59f03a80268375f90b209d30e5a46929eb1b06a3
atg-abhijay <atg.abhijay@gmail.com>
2018-08-28 20:47:34 -0400

    solved 461: Hamming Distance - have to try and improve runtime

--- hamming_distance_461.py
+++ hamming_distance_461.py
@@ -2,7 +2,16 @@
 URL of problem: https://leetcode.com/problems/hamming-distance/description/
 """
 
-def main():
-    pass
+def main(x, y):
+    bin_x = int(bin(x), 2)
+    bin_y = int(bin(y), 2)
+    xor_result = bin(bin_x ^ bin_y)
+    hamming_dist = 0
+    for digit in xor_result:
+        if digit == '1':
+            hamming_dist += 1
 
-main()
+    # print("Hamming distance:", hamming_dist)
+    return hamming_dist
+
+main(int(input("Give first number: ")), int(input("Give second number: ")))

c9117b188588808315d7d76307270bcc300bf1ca
atg-abhijay <atg.abhijay@gmail.com>
2018-08-29 20:51:49 -0400

    7: added comments

--- reverse_int_7.py
+++ reverse_int_7.py
@@ -7,14 +7,22 @@ def main(x):
     reversed_num = 0
     if x < 0:
         str_num = str(x)
+        # dropping the '-' sign
         num_as_list = list(str_num)[1:]
         reverse_num_list = reversed(num_as_list)
+        # the numbers are stored as chars. we join
+        # them first, convert to an int and multiply by (-1)
         reversed_num = (-1) * int(''.join(reverse_num_list))
     else:
+        # convert:
+        # num -> str -> list
+        # reverse list
+        # list -> int
         str_num = str(x)
         num_as_list = list(str_num)
         reversed_num = int(''.join(reversed(num_as_list)))
 
+    # max_int = 2^31 - 1
     max_int = 2147483647
     if reversed_num < -1-max_int or reversed_num > max_int:
         print(0)

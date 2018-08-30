ffa847b63a56ceb1b2f87306bae5385c1b5cc63f
atg-abhijay <atg.abhijay@gmail.com>
2018-08-29 20:05:25 -0400

    561: added comments

--- array_partition_i_561.py
+++ array_partition_i_561.py
@@ -3,6 +3,19 @@ URL of problem: https://leetcode.com/problems/array-partition-i/description/
 """
 
 def main(nums):
+    # the way to achieve the
+    # maximum sum is to -
+    # sort the numbers and take
+    # the sum of alternate numbers
+    # starting from the very first number
+
+    # our ideal sum would have been the
+    # sum of the second set of n numbers.
+    # but that cannot be achieved with the
+    # given criteria so this way allows us
+    # to get half of those second set of n
+    # numbers and the other half comes from
+    # the first set of n numbers
     sorted_nums = sorted(nums)
     max_sum = 0
     length = int(len(sorted_nums)/2)

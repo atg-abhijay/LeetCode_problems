d6c068c761d5447d49b18a44f748c5eaed551d60
atg-abhijay <atg.abhijay@gmail.com>
2018-08-29 20:01:40 -0400

    solved 561: Array Partition I

--- array_partition_i_561.py
+++ array_partition_i_561.py
@@ -2,9 +2,15 @@
 URL of problem: https://leetcode.com/problems/array-partition-i/description/
 """
 
-def main():
-    pass
+def main(nums):
+    sorted_nums = sorted(nums)
+    max_sum = 0
+    length = int(len(sorted_nums)/2)
+    for i in range(length):
+        max_sum += sorted_nums[2*i]
 
+    print("Maximum sum:", max_sum)
+    # return max_sum
 
 if __name__ == '__main__':
-    main()
+    main([1, 4, 3, 2])

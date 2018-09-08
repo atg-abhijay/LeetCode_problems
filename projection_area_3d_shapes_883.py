cb314e5681d693c95e7ad1f02d11022ca7c00ee4
atg-abhijay <atg.abhijay@gmail.com>
2018-09-08 19:55:00 -0400

    solved 883: Projection Area of 3D Shapes - removed commented code, added comments and docstrings

--- projection_area_3d_shapes_883.py
+++ projection_area_3d_shapes_883.py
@@ -1,43 +1,31 @@
 """
-URL of problem: https://leetcode.com/problems/projection-area-of-3d-shapes/description/
+URL of problem:
+https://leetcode.com/problems/projection-area-of-3d-shapes/description/
 """
 
+
 def main(grid):
+    """
+    main method for running the program.
+    Argument:
+        grid - a 2D grid with grid[i][j] = height of tower
+    Prints:
+        Total projection area
+    """
     dimension = len(grid)
 
-    # yz_dict = {}
-    # for i in range(dimension):
-    #     column = grid[i]
-    #     for j in range(dimension):
-    #         z_val = column[j]
-
+    # dict where:
+    # Key - tuple (x, y)
+    # Value - height of tower
     xyz_dict = {}
     for i in range(dimension):
         for j in range(dimension):
-            xyz_dict[(i,j)] = grid[i][j]
+            xyz_dict[(i, j)] = grid[i][j]
 
-    # yz_dict = {}
-    # for key, z_val in xyz_dict.items():
-    #     y_val = key[1]
-    #     if y_val in yz_dict:
-    #         yz_dict[y_val].append(z_val)
-    #     else:
-    #         yz_dict[y_val] = [z_val]
-
-    # yz_area = 0
-    # for list_val in yz_dict.values():
-    #     yz_area += max(list_val)
-
-    # xz_dict = {}
-    # for key, z_val in xyz_dict.items():
-    #     x_val = key[0]
-    #     if x_val in xz_dict:
-    #         xz_dict[x_val].append(z_val)
-    #     else:
-    #         xz_dict[x_val] = x_val
-
-    # print(yz_dict)
-
+    # for xy_area we simply look at the
+    # presence/absence of a tower. we don't
+    # care about the height itself. if a tower
+    # is present, then add 1 to the xy_area
     xy_area = 0
     for val in xyz_dict.values():
         if val != 0:
@@ -51,12 +39,34 @@ def main(grid):
 
     total_projection = xy_area + yz_area + xz_area
     print("Total projection:", total_projection)
+    # return total_projection
 
 
 def make_dict(xyz_dict, key_index):
+    """
+    Makes a dictionary where:
+        Key - 'a'-value ('a' can be x or y)
+        Value - list of all the z-values associated
+                with that specific a-value
+    Arguments:
+        xyz_dict - dict with:
+                    Key - tuple (x, y)
+                    Value - the z-value for the tuple
+        key_index - takes on a value of 0 or 1
+                    for x or y in the tuple (x, y)
+    Returns:
+        a dictionary az ('a' can be x or y) where:
+            Key - 'a'-value ('a' can be x or y)
+            Value - list of all the z-values associated
+                    with that specific a-value
+    """
     az_dict = {}
     for key, z_val in xyz_dict.items():
         a_val = key[key_index]
+        # if a_val already exists in dict,
+        # then add the z_val to its list of
+        # values else initialize the list
+        # for it with this z_val
         if a_val in az_dict:
             az_dict[a_val].append(z_val)
         else:
@@ -66,6 +76,24 @@ def make_dict(xyz_dict, key_index):
 
 
 def calc_area(az_dict):
+    """
+    For each of the coordinate values
+    'a' (can be x or y), find the maximum
+    value in its list of values and add
+    that to the projection area on the
+    az-plane.
+
+    The projection area along each 'a'
+    coordinate is the highest tower, therefore
+    we take the maximum value for each coordinate.
+
+    Argument:
+        az_dict - dict with
+                    Key - 'a'-value
+                    Value - list of z-values
+    Returns:
+        Area of projection on the az-plane
+    """
     az_area = 0
     for list_val in az_dict.values():
         az_area += max(list_val)
@@ -74,4 +102,4 @@ def calc_area(az_dict):
 
 
 if __name__ == '__main__':
-    main([[1,2],[3,4]])
+    main([[1, 2], [3, 4]])

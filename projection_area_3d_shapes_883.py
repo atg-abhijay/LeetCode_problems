7e2e976d7060772a3243bc6b6856b5613ebfe7a8
atg-abhijay <atg.abhijay@gmail.com>
2018-09-08 18:28:59 -0400

    883: working with example inputs

--- projection_area_3d_shapes_883.py
+++ projection_area_3d_shapes_883.py
@@ -2,9 +2,72 @@
 URL of problem: https://leetcode.com/problems/projection-area-of-3d-shapes/description/
 """
 
-def main():
-    pass
+def main(grid):
+    dimension = len(grid)
+    xy_area = dimension*dimension
+
+    # yz_dict = {}
+    # for i in range(dimension):
+    #     column = grid[i]
+    #     for j in range(dimension):
+    #         z_val = column[j]
+
+    xyz_dict = {}
+    for i in range(dimension):
+        for j in range(dimension):
+            xyz_dict[(i,j)] = grid[i][j]
+
+    # yz_dict = {}
+    # for key, z_val in xyz_dict.items():
+    #     y_val = key[1]
+    #     if y_val in yz_dict:
+    #         yz_dict[y_val].append(z_val)
+    #     else:
+    #         yz_dict[y_val] = [z_val]
+
+    # yz_area = 0
+    # for list_val in yz_dict.values():
+    #     yz_area += max(list_val)
+
+    # xz_dict = {}
+    # for key, z_val in xyz_dict.items():
+    #     x_val = key[0]
+    #     if x_val in xz_dict:
+    #         xz_dict[x_val].append(z_val)
+    #     else:
+    #         xz_dict[x_val] = x_val
+
+    # print(yz_dict)
+
+    yz_dict = make_dict(xyz_dict, 1)
+    yz_area = calc_area(yz_dict)
+
+    xz_dict = make_dict(xyz_dict, 0)
+    xz_area = calc_area(xz_dict)
+
+    total_projection = xy_area + yz_area + xz_area
+    print("Total projection:", total_projection)
 
 
-if __name__ = '__main__':
-    main()
+def make_dict(xyz_dict, key_index):
+    az_dict = {}
+    for key, z_val in xyz_dict.items():
+        a_val = key[key_index]
+        if a_val in az_dict:
+            az_dict[a_val].append(z_val)
+        else:
+            az_dict[a_val] = [z_val]
+
+    return az_dict
+
+
+def calc_area(az_dict):
+    az_area = 0
+    for list_val in az_dict.values():
+        az_area += max(list_val)
+
+    return az_area
+
+
+if __name__ == '__main__':
+    main([[1,2],[3,4]])

cbb9fc28a8583d1179afc5af5e0f7b90ad26434f
atg-abhijay <atg.abhijay@gmail.com>
2018-09-13 19:51:01 -0400

    476: Number Complement - initial commit

---
+++ number_complement_476.py
@@ -0,0 +1,12 @@
+"""
+URL of problem:
+https://leetcode.com/problems/number-complement/description/
+"""
+
+
+def main(num):
+    pass
+
+
+if __name__ == '__main__':
+    main(int(input("Enter a number: ")))

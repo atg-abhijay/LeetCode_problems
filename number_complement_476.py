96363f128afed11aef12ff2cd7fd95b673b1bca6
atg-abhijay <atg.abhijay@gmail.com>
2018-09-13 20:15:07 -0400

    476: removed unwanted lines

--- number_complement_476.py
+++ number_complement_476.py
@@ -6,17 +6,15 @@ https://leetcode.com/problems/number-complement/description/
 
 def main(num):
     bin_num = bin(num)[2:]
-    # print(bin_num)
     complement_list = [1 - int(digit) for digit in bin_num]
-    # print(complement_list)
     complement_list.reverse()
     complement = 0
     for power, bit in enumerate(complement_list):
         if bit != 0:
             complement += 2**power
 
-    # print("Complement:", complement)
-    return complement
+    print("Complement:", complement)
+    # return complement
 
 
 if __name__ == '__main__':

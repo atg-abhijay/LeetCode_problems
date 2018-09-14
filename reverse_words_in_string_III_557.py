2f1d1a29bda1d255434719301c49a32f33f412e0
atg-abhijay <atg.abhijay@gmail.com>
2018-09-13 23:40:42 -0400

    557: added commemts

--- reverse_words_in_string_III_557.py
+++ reverse_words_in_string_III_557.py
@@ -5,15 +5,30 @@ https://leetcode.com/problems/reverse-words-in-a-string-iii/description/
 
 
 def main(st):
+    """
+    main method for running the program.
+    Argument:
+        st - any string
+    Prints:
+        Reverse the order of characters in each word
+        within a sentence while still preserving
+        whitespace and initial word order.
+    """
+    # separate the words
     st_list = st.split(' ')
     output = []
     for word in st_list:
+        # convert the word to a list of chars,
+        # reverse the list, combine the list
+        # to get the reversed word and add
+        # that word to the 'output' list
         w_list = list(word)
         w_list.reverse()
         output.append(''.join(w_list))
 
-    # output = [''.join(list(word).reverse()) for word in st_list]
+    # join the words in the output list
     print(' '.join(output))
+    # return ' '.join(output)
 
 
 if __name__ == '__main__':

530b623883b3221692883b5651a9694532871fe8
atg-abhijay <atg.abhijay@gmail.com>
2018-09-13 23:34:32 -0400

    solved 557: Reverse Words in a String III

--- reverse_words_in_string_III_557.py
+++ reverse_words_in_string_III_557.py
@@ -4,9 +4,17 @@ https://leetcode.com/problems/reverse-words-in-a-string-iii/description/
 """
 
 
-def main():
-    pass
+def main(st):
+    st_list = st.split(' ')
+    output = []
+    for word in st_list:
+        w_list = list(word)
+        w_list.reverse()
+        output.append(''.join(w_list))
+
+    # output = [''.join(list(word).reverse()) for word in st_list]
+    print(' '.join(output))
 
 
 if __name__ == '__main__':
-    main()
+    main(input("Input a string: "))

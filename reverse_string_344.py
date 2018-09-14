833a601b471c60e5c57d6deb27c50b78efa7928c
atg-abhijay <atg.abhijay@gmail.com>
2018-09-13 23:15:48 -0400

    solved 344: Reverse String

--- reverse_string_344.py
+++ reverse_string_344.py
@@ -4,9 +4,11 @@ https://leetcode.com/problems/reverse-string/description/
 """
 
 
-def main():
-    pass
+def main(string):
+    s_list = list(string)
+    s_list.reverse()
+    print(''.join(s_list))
 
 
 if __name__ == '__main__':
-    main()
+    main(input("Input a string: "))

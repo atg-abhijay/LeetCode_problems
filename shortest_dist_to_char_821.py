e6814d02300f9c4f49011164d52e9c4bb911cedc
atg-abhijay <atg.abhijay@gmail.com>
2018-09-15 15:01:46 -0400

    solved 821: Shortest Distance to a Character

--- shortest_dist_to_char_821.py
+++ shortest_dist_to_char_821.py
@@ -4,9 +4,24 @@ https://leetcode.com/problems/shortest-distance-to-a-character/description/
 """
 
 
-def main():
-    pass
+def main(string, char):
+    char_positions = []
+    for index, letter in enumerate(string):
+        if letter == char:
+            char_positions.append(index)
 
+    char_distances = []
+    for index, letter in enumerate(string):
+        min_dist = len(string)
+        for pos in char_positions:
+            dist = abs(index - pos)
+            if dist < min_dist:
+                min_dist = dist
+
+        char_distances.append(min_dist)
+
+    print(char_distances)
+    # return char_distances
 
 if __name__ == '__main__':
-    main()
+    main("loveleetcode", 'e')

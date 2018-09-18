f141f353ad7321bf9e5bafa624d14ee5a0cf11fb
atg-abhijay <atg.abhijay@gmail.com>
2018-09-18 01:05:18 -0400

    solved 868: Binary Gap

--- binary_gap_868.py
+++ binary_gap_868.py
@@ -4,9 +4,38 @@ https://leetcode.com/problems/binary-gap/description/
 """
 
 
-def main():
-    pass
+def main(num):
+    bin_num = bin(num)[2:]
+    max_dist = 0
+    dist_counter = -1
+    encounter_start_one = False
+    # encounter_end_one = False
+    for digit in bin_num:
+        digit = int(digit)
+        if encounter_start_one:
+            if digit == 1:
+                dist_counter += 1
+                # encounter_end_one = True
+                if max_dist < dist_counter:
+                    max_dist = dist_counter
+
+                dist_counter = 0
+            else:
+                dist_counter += 1
+
+        else:
+            if digit == 1:
+                encounter_start_one = True
+                dist_counter += 1
+
+
+        # if encounter_start_one and int(digit) == 1:
+        #     encounter_start_one = True
+        #     dist_counter += 1
+
+    print("Max distance:", max_dist)
 
 
 if __name__ == '__main__':
-    main()
+    # main(int(input("Give a number: ")))
+    main(22)

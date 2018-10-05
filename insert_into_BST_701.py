77a6a5a84d34f4235d0e2efd8e3fa5f830b508b7
atg-abhijay <atg.abhijay@gmail.com>
2018-10-05 01:02:07 -0400

    701: solved Insert into a Binary Search Tree

--- insert_into_BST_701.py
+++ insert_into_BST_701.py
@@ -18,3 +18,15 @@ class Solution(object):
         :type val: int
         :rtype: TreeNode
         """
+        if val < root.val:
+            if root.left is None:
+                root.left = TreeNode(val)
+            else:
+                self.insertIntoBST(root.left, val)
+        else:
+            if root.right is None:
+                root.right = TreeNode(val)
+            else:
+                self.insertIntoBST(root.right, val)
+
+        return root

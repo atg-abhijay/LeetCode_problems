5a0413f472ca450fcbf5befced1229e8d16e1480
atg-abhijay <atg.abhijay@gmail.com>
2018-10-05 01:07:36 -0400

    654: Maximum Binary Tree - initial commit

---
+++ maximum_binary_tree_654.py
@@ -0,0 +1,20 @@
+"""
+URL of problem:
+https://leetcode.com/problems/maximum-binary-tree/description/
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def constructMaximumBinaryTree(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """

044000118a7174a8f9bb255a41d2fc33de9327f7
atg-abhijay <atg.abhijay@gmail.com>
2018-10-08 15:02:10 -0400

    463: added comments

--- island_perimeter_463.py
+++ island_perimeter_463.py
@@ -5,6 +5,10 @@ https://leetcode.com/problems/island-perimeter/description/
 
 
 def main(grid):
+    # adding water cells/blocks around
+    # the entire grid. now, we don't have
+    # to check edge cases where there isn't
+    # a block adjacent to another block.
     for i in range(len(grid)):
         grid[i] = [0] + grid[i] + [0]
 
@@ -15,7 +19,10 @@ def main(grid):
     perimeter = 0
     for i in range(1, len(grid)-1):
         for j in range(1, len(grid[0])-1):
+            # if the cell is land
             if grid[i][j] == 1:
+                # if the land is surrounded
+                # by water, increment perimeter
                 if grid[i-1][j] == 0:
                     perimeter += 1
                 if grid[i][j+1] == 0:

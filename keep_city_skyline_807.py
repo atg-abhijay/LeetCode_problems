57ffb50f70e433f5d16d9109d301448708b48053
atg-abhijay <atg.abhijay@gmail.com>
2018-10-08 01:39:10 -0400

    807: added comments

--- keep_city_skyline_807.py
+++ keep_city_skyline_807.py
@@ -5,9 +5,11 @@ https://leetcode.com/problems/max-increase-to-keep-city-skyline/description/
 
 
 def main(grid):
+    # skyline values by columns
     skyline_by_cols = []
     grid_length = len(grid)
 
+    # making a transpose of the grid
     grid_transpose = [[0 for x in range(grid_length)]
                       for y in range(grid_length)]
 
@@ -16,15 +18,18 @@ def main(grid):
         for j in range(grid_length):
             grid_transpose[j][i] = row[j]
 
+    # adding skyline by column values
     for row in grid_transpose:
         skyline_by_cols.append(max(row))
 
-    # print(grid_transpose)
     max_increase = 0
     for i in range(grid_length):
         row_max = max(grid[i])
         for j in range(grid_length):
             col_max = skyline_by_cols[j]
+            # the grid value at the max can be the minimum
+            # amongst the skyline row and column value
+            # at that intersection
             max_increase += min(row_max, col_max) - grid[i][j]
             grid[i][j] = min(row_max, col_max)
 

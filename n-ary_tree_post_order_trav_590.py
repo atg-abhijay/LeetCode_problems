405fd8ed3299e61e20e649d71b00dc22eba08f5b
atg-abhijay <atg.abhijay@gmail.com>
2018-10-08 19:12:33 -0400

    590: N-ary Tree Postorder Traversal - initial commit

---
+++ n-ary_tree_post_order_trav_590.py
@@ -0,0 +1,19 @@
+"""
+URL of problem:
+https://leetcode.com/problems/n-ary-tree-postorder-traversal/description/
+"""
+
+
+# Definition for a Node.
+# class Node(object):
+#     def __init__(self, val, children):
+#         self.val = val
+#         self.children = children
+
+
+class Solution(object):
+    def postorder(self, root):
+        """
+        :type root: Node
+        :rtype: List[int]
+        """

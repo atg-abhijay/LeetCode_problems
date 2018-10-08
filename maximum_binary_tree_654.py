9ad2462c7539f8f03f74777b4a0e658cccb0c116
atg-abhijay <atg.abhijay@gmail.com>
2018-10-08 02:13:33 -0400

    654: removed unwanted code and added comments

--- maximum_binary_tree_654.py
+++ maximum_binary_tree_654.py
@@ -5,11 +5,11 @@ https://leetcode.com/problems/maximum-binary-tree/description/
 
 
 # Definition for a binary tree node.
-class TreeNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
 
 class Solution(object):
@@ -29,13 +29,11 @@ class Solution(object):
         left_nums = nums[:max_idx]
         right_nums = nums[max_idx+1:]
 
+        # calling the method on the 2 lists
+        # to construct the left and right
+        # subtrees. the values returned form
+        # the children of the root node.
         root.left = self.constructMaximumBinaryTree(left_nums)
         root.right = self.constructMaximumBinaryTree(right_nums)
 
         return root
-
-    def test():
-        nums = [4, 5, 6]
-        print(nums[3:])
-
-    constructMaximumBinaryTree(self, [3,2,1,6,0,5])

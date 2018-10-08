eaf5fc8548de42290f6227d6be85f7a8da2c81bf
atg-abhijay <atg.abhijay@gmail.com>
2018-10-08 01:08:08 -0400

    807: extracting max heights along rows and columsn

--- keep_city_skyline_807.py
+++ keep_city_skyline_807.py
@@ -4,9 +4,28 @@ https://leetcode.com/problems/max-increase-to-keep-city-skyline/description/
 """
 
 
-def main():
-    pass
+def main(grid):
+    skyline_by_rows = []
+    skyline_by_cols = []
+    grid_length = len(grid)
+
+    for row in grid:
+        skyline_by_rows.append(max(row))
+
+    grid_transpose = [[0 for x in range(grid_length)]
+                      for y in range(grid_length)]
+    for i in range(grid_length):
+        row = grid[i]
+        for j in range(grid_length):
+            grid_transpose[j][i] = row[j]
+
+    for row in grid_transpose:
+        skyline_by_cols.append(max(row))
+
+
+
+    # print(grid_transpose)
 
 
 if __name__ == '__main__':
-    main()
+    main([[1, 2, 3], [4, 5, 6], [7, 8, 9]])

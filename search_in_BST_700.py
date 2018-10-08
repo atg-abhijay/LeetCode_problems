8bcdc4eb49b2744c3bf927378ed0cacc161fc610
atg-abhijay <atg.abhijay@gmail.com>
2018-10-08 12:32:14 -0400

    700: Search in a BST - initial commit

---
+++ search_in_BST_700.py
@@ -0,0 +1,20 @@
+"""
+URL of problem:
+https://leetcode.com/problems/search-in-a-binary-search-tree/description/
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def searchBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """

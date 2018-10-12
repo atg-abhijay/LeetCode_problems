60d097c4e811583d9f2269b87b318a7d881adb8a
atg-abhijay <atg.abhijay@gmail.com>
2018-10-12 12:15:50 -0400

    814: removed additional checks on left and right children

--- binary_tree_pruning_814.py
+++ binary_tree_pruning_814.py
@@ -19,10 +19,8 @@ class Solution(object):
         :rtype: TreeNode
         """
         if root:
-            if root.left:
-                root.left = self.pruneTree(root.left)
-            if root.right:
-                root.right = self.pruneTree(root.right)
+            root.left = self.pruneTree(root.left)
+            root.right = self.pruneTree(root.right)
 
             if root.val == 0 and root.left is None and root.right is None:
                 root = None

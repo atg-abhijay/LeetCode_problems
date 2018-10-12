85c70bbcf6cbb459a4baec269afb38b5bbb7b6d5
atg-abhijay <atg.abhijay@gmail.com>
2018-10-12 11:37:08 -0400

    890: added comments

--- find_and_replace_pattern_890.py
+++ find_and_replace_pattern_890.py
@@ -10,19 +10,38 @@ def main(words, pattern):
         mapping = {}
         list_chars = [char for char in word]
         list_pattern = [char for char in pattern]
+        # assume that the word
+        # matches the given pattern
         shouldAdd = True
+        # iterating character by character
+        # over the word and the pattern
         for w, p in zip(list_chars, list_pattern):
+            # if the word_letter has
+            # not been added to the map
             if w not in mapping:
+                # if the word_letter maps to a
+                # pattern_letter which is already
+                # assigned to another word_letter,
+                # the word does not match the pattern
                 if p in mapping.values():
                     shouldAdd = False
                     break
                 else:
                     mapping[w] = p
+
+            # word_letter exists in the map.
+            # if pattern_letter doesn't match
+            # the pattern_letter assigned to
+            # this word_letter, the word does
+            # not match the pattern
             else:
                 if p != mapping[w]:
                     shouldAdd = False
                     break
 
+        # if shouldAdd was not negated
+        # anywhere in the loop above,
+        # the word matches the pattern
         if shouldAdd:
             match_pattern.append(word)
 

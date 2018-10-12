6439b15a9a74d869d23e9badbc1b7b5e3c01936c
atg-abhijay <atg.abhijay@gmail.com>
2018-10-11 20:36:56 -0400

    419: added coments

--- battleships_in_board_419.py
+++ battleships_in_board_419.py
@@ -11,9 +11,22 @@ def main(board):
     for i in range(num_rows):
         row = board[i]
         for j in range(num_cols):
+            # bship = battleship
+            # if the cell is marked 'X',
+            # change it to 'A' and since
+            # we have a bship for
+            # sure, increment num_battleships.
             if row[j] == 'X':
                 row[j] = 'A'
                 num_battleships += 1
+                # we have two possibilites now. the
+                # bship might be horizontal or vertical.
+                # assume it to not be horizontal. check
+                # the cell to the right of the current
+                # cell. if it is marked 'X' we have a
+                # horizontal bship. check how long we
+                # obtain X's for and mark them with A's
+                # so that we don't check those cells later
                 isHorizontal = False
                 j_inner = j+1
                 while j_inner < num_cols:
@@ -24,6 +37,12 @@ def main(board):
                     else:
                         break
 
+                # if the previous loop did
+                # not set isHorizontal to
+                # True, then the bship is vertical.
+                # follow a similar procedure as above,
+                # checking cells below the current cell
+                # until we stop getting X's.
                 if not isHorizontal:
                     i_inner = i+1
                     while i_inner < num_rows:

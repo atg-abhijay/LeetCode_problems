faad924d4647c8e3340802e318cea8ae6aa641fb
atg-abhijay <atg.abhijay@gmail.com>
2018-10-11 22:11:07 -0400

    797: working partially

--- all_paths_source_to_target_797.py
+++ all_paths_source_to_target_797.py
@@ -4,9 +4,27 @@ https://leetcode.com/problems/all-paths-from-source-to-target/description/
 """
 
 
-def main():
-    pass
+def main(graph):
+    all_paths = traverse(graph, 0)
+    for path in all_paths:
+        print(path)
+
+
+def traverse(graph, node):
+    all_paths = []
+    path = []
+    path.append(node)
+    for child in graph[node]:
+        path += traverse(graph, child)
+        if node == 0 and path[-1] == len(graph) - 1:
+            all_paths.append(path)
+            path = [0]
+
+    if node != 0:
+        return path
+    else:
+        return all_paths
 
 
 if __name__ == '__main__':
-    main()
+    main([[4, 3, 1], [3, 2, 4], [3], [4], []])

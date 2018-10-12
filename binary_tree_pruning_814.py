ca37d9b6981d41bd116b06186a7ad2d6845ae07e
atg-abhijay <atg.abhijay@gmail.com>
2018-10-12 11:40:57 -0400

    814: Binary Tree Pruning - initial commit

---
+++ binary_tree_pruning_814.py
@@ -0,0 +1,20 @@
+"""
+URL of problem:
+https://leetcode.com/problems/binary-tree-pruning/description/
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution(object):
+    def pruneTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """

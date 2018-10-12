16ce54f08794a599418d865911a17d22e0e06624
atg-abhijay <atg.abhijay@gmail.com>
2018-10-12 12:13:07 -0400

    solved 814: Binary Tree Pruning

--- binary_tree_pruning_814.py
+++ binary_tree_pruning_814.py
@@ -18,3 +18,13 @@ class Solution(object):
         :type root: TreeNode
         :rtype: TreeNode
         """
+        if root:
+            if root.left:
+                root.left = self.pruneTree(root.left)
+            if root.right:
+                root.right = self.pruneTree(root.right)
+
+            if root.val == 0 and root.left is None and root.right is None:
+                root = None
+
+        return root

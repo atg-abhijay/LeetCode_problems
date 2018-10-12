f3f403e8a578c15556cc827bd1e2c1129089dd35
atg-abhijay <atg.abhijay@gmail.com>
2018-10-12 10:54:54 -0400

    solved 797: All Paths From Source to Target (traverse2())

--- all_paths_source_to_target_797.py
+++ all_paths_source_to_target_797.py
@@ -5,25 +5,45 @@ https://leetcode.com/problems/all-paths-from-source-to-target/description/
 
 
 def main(graph):
-    all_paths = traverse(graph, 0)
+    # all_paths = traverse(graph, 0)
+    all_paths = traverse2(graph, 0, [], len(graph)-1)
     for path in all_paths:
         print(path)
 
 
 def traverse(graph, node):
-    all_paths = []
-    path = []
+    all_paths, path = [], []
     path.append(node)
+    # stack.append(node)
     for child in graph[node]:
+        # result = traverse(graph, child)
         path += traverse(graph, child)
+        # stack = result[1]
         if node == 0 and path[-1] == len(graph) - 1:
             all_paths.append(path)
             path = [0]
 
     if node != 0:
-        return path
+        # stack.pop()
+        return [path, stack]
     else:
         return all_paths
+
+
+def traverse2(graph, node, path, last_node):
+    all_paths = []
+    path.append(node)
+    if not graph[node]:
+        if node == last_node:
+            all_paths.append(path)
+            return all_paths
+
+    else:
+        for child in graph[node]:
+            path_to_send = [elem for elem in path]
+            all_paths += traverse2(graph, child, path_to_send, last_node)
+
+    return all_paths
 
 
 if __name__ == '__main__':

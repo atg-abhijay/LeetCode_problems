91e3b068a86b4ddb3fe63c66e380d15df4b2db3a
atg-abhijay <atg.abhijay@gmail.com>
2018-10-12 11:31:02 -0400

    solved 890: Find and Replace Pattern

--- find_and_replace_pattern_890.py
+++ find_and_replace_pattern_890.py
@@ -4,9 +4,31 @@ https://leetcode.com/problems/find-and-replace-pattern/description/
 """
 
 
-def main():
-    pass
+def main(words, pattern):
+    match_pattern = []
+    for word in words:
+        mapping = {}
+        list_chars = [char for char in word]
+        list_pattern = [char for char in pattern]
+        shouldAdd = True
+        for w, p in zip(list_chars, list_pattern):
+            if w not in mapping:
+                if p in mapping.values():
+                    shouldAdd = False
+                    break
+                else:
+                    mapping[w] = p
+            else:
+                if p != mapping[w]:
+                    shouldAdd = False
+                    break
+
+        if shouldAdd:
+            match_pattern.append(word)
+
+    print(match_pattern)
+    # return match_pattern
 
 
 if __name__ == '__main__':
-    main()
+    main(words=["abc", "deq", "mee", "aqq", "dkd", "ccc"], pattern="abb")

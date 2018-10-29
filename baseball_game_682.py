ebff78532b590c9d46d6ed03fba734cd50950152
atg-abhijay <atg.abhijay@gmail.com>
2018-10-29 17:31:16 -0400

    solved 682: Baseball Game

--- baseball_game_682.py
+++ baseball_game_682.py
@@ -4,9 +4,31 @@ https://leetcode.com/problems/baseball-game/description/
 """
 
 
-def main():
-    pass
+def main(operations):
+    total_points = 0
+    points = []
+    for op in operations:
+        if op == "+":
+            new_points = points[-1] + points[-2]
+            total_points += new_points
+            points.append(new_points)
+
+        elif op == "D":
+            new_points = 2*points[-1]
+            total_points += new_points
+            points.append(new_points)
+
+        elif op == "C":
+            total_points -= points[-1]
+            points.pop()
+
+        else:
+            total_points += int(op)
+            points.append(int(op))
+
+    # print("Total points:", total_points)
+    return total_points
 
 
 if __name__ == '__main__':
-    main()
+    main(["5", "-2", "4", "C", "D", "9", "+", "+"])

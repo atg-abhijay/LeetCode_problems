0d0a9813617072368e2bbed4ac75ccc7fcdd0234
atg-abhijay <atg.abhijay@gmail.com>
2018-11-16 23:56:13 -0500

    938: Range Sum of BST - initial commit

---
+++ range_sum_bst_938.py
@@ -0,0 +1,20 @@
+"""
+URL of problem:
+https://leetcode.com/problems/range-sum-of-bst/
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def rangeSumBST(self, root, L, R):
+        """
+        :type root: TreeNode
+        :type L: int
+        :type R: int
+        :rtype: int
+        """

8d61c4239866ba2eb1bb2801f9d8c1a55a967bf2
atg-abhijay <atg.abhijay@gmail.com>
2018-12-21 17:31:57 -0500

    931: Minimum Falling Path Sum- initial commit

---
+++ MinFallingPathSum931.java
@@ -0,0 +1,18 @@
+/**
+ * URL of problem:
+ * https://leetcode.com/problems/minimum-falling-path-sum/
+ */
+
+public class MinFallingPathSum931 {
+    public static void main(String[] args) {
+        Solution931 sol = new Solution931();
+        int minSum = sol.minFallingPathSum(A);
+        System.out.println(minSum);
+    }
+}
+
+class Solution931 {
+    public int minFallingPathSum(int[][] A) {
+        return -1;
+    }
+}

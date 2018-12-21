7db47ffe5eb878315ab33c0d3de01c24c2f440b3
atg-abhijay <atg.abhijay@gmail.com>
2018-12-21 17:17:53 -0500

    338: added comments. solved in java. deleting python file. runtime: java (2 ms) vs python (152 ms)

--- CountingBits338.java
+++ CountingBits338.java
@@ -1,3 +1,8 @@
+/**
+ * URL of problem:
+ * https://leetcode.com/problems/counting-bits/
+ */
+
 public class CountingBits338 {
     public static void main(String[] args) {
         Solution338 sol = new Solution338();
@@ -14,10 +19,19 @@ class Solution338 {
         int[] numBits = new int[num+1];
         boolean odd = true;
         for(int i = 1; i <= num; i++) {
+            /**
+             * if i is odd, it's #1bits is
+             * (1 + #1bits in the previous number)
+             */
             if(odd) {
                 numBits[i] = numBits[i-1] + 1;
                 odd = false;
             }
+            /**
+             * if i is even, it can be obtained
+             * by (i/2)*2, which doesn't add
+             * any extra 1 bits to i
+             */
             else {
                 numBits[i] = numBits[i/2];
                 odd = true;

b74f29dc06a1db2dc53afc92bf40965a1cc7a857
atg-abhijay <atg.abhijay@gmail.com>
2018-12-21 21:27:57 -0500

    solved 70: Climbing Stairs

--- ClimbingStairs70.java
+++ ClimbingStairs70.java
@@ -6,12 +6,21 @@
 public class ClimbingStairs70 {
     public static void main(String[] args) {
         Solution70 sol = new Solution70();
-        Sysout(sol.climbStairs(n));
+        System.out.println(sol.climbStairs(3));
     }
 }
 
 class Solution70 {
     public int climbStairs(int n) {
-        return -1;
+        int[] numWays = new int[n+1];
+        numWays[0] = 1;
+        numWays[1] = 1;
+        if(n == 1) {
+            return 1;
+        }
+        for(int i = 2; i < n+1; i++) {
+            numWays[i] = numWays[i-2] + numWays[i-1];
+        }
+        return numWays[n];
     }
 }

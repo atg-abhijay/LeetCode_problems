807803c93b34a8286303aa34190615fd385c0695
atg-abhijay <atg.abhijay@gmail.com>
2018-12-21 21:04:23 -0500

    70: Climbing Stairs - initial commit

---
+++ ClimbingStairs70.java
@@ -0,0 +1,17 @@
+/**
+ * URL of problem:
+ * https://leetcode.com/problems/climbing-stairs/
+ */
+
+public class ClimbingStairs70 {
+    public static void main(String[] args) {
+        Solution70 sol = new Solution70();
+        Sysout(sol.climbStairs(n));
+    }
+}
+
+class Solution70 {
+    public int climbStairs(int n) {
+        return -1;
+    }
+}

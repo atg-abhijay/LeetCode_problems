629dfcaf65a7734e87508b662c1354dc4cc72643
atg-abhijay <atg.abhijay@gmail.com>
2018-12-23 18:08:32 -0500

    746: Minimum Cost Climbing Stairs - initial commit

---
+++ MinCostClimbingStairs746.java
@@ -0,0 +1,18 @@
+/**
+ * URL of problem:
+ * https://leetcode.com/problems/min-cost-climbing-stairs/
+ */
+
+public class MinCostClimbingStairs746 {
+    public static void main(String[] args) {
+        Solution746 sol = new Solution746();
+        int[] cost = {};
+        System.out.println(sol.minCostClimbingStairs(cost));
+    }
+}
+
+class Solution746 {
+    public int minCostClimbingStairs(int[] cost) {
+        return -1;
+    }
+}

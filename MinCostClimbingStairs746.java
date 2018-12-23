e642b80124781b11a229ea04d54d34846b4e5820
atg-abhijay <atg.abhijay@gmail.com>
2018-12-23 18:19:29 -0500

    solved 746: Min Cost Climbing Stairs

--- MinCostClimbingStairs746.java
+++ MinCostClimbingStairs746.java
@@ -6,13 +6,19 @@
 public class MinCostClimbingStairs746 {
     public static void main(String[] args) {
         Solution746 sol = new Solution746();
-        int[] cost = {};
+        int[] cost = {1, 100, 1, 1, 1, 100, 1, 1, 100, 1};
         System.out.println(sol.minCostClimbingStairs(cost));
     }
 }
 
 class Solution746 {
     public int minCostClimbingStairs(int[] cost) {
-        return -1;
+        int[] minCosts = new int[cost.length+1];
+        minCosts[0] = 0;
+        minCosts[1] = 0;
+        for(int i = 2; i < minCosts.length; i++) {
+            minCosts[i] = Math.min(cost[i-2] + minCosts[i-2], cost[i-1] + minCosts[i-1]);
+        }
+        return minCosts[minCosts.length-1];
     }
 }

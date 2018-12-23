3447ff5126f32c2d1fc9768e533c8fddf41a021d
atg-abhijay <atg.abhijay@gmail.com>
2018-12-23 18:53:49 -0500

    solved 198: House Robber

--- HouseRobber198.java
+++ HouseRobber198.java
@@ -6,13 +6,22 @@
 public class HouseRobber198 {
     public static void main(String[] args) {
         Solution198 sol = new Solution198();
-        int[] nums = {};
+        int[] nums = {2, 7, 9, 3, 1};
         System.out.println(sol.rob(nums));
     }
 }
 
 class Solution198 {
     public int rob(int[] nums) {
-        return -1;
+        if(nums.length == 0) {
+            return 0;
+        }
+        int[] maxAmountRob = new int[nums.length+1];
+        maxAmountRob[0] = 0;
+        maxAmountRob[1] = nums[0];
+        for(int i = 2; i < maxAmountRob.length; i++) {
+            maxAmountRob[i] = Math.max(maxAmountRob[i-1], nums[i-1] + maxAmountRob[i-2]);
+        }
+        return maxAmountRob[maxAmountRob.length-1];
     }
 }

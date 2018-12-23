8da9d4d962785c02bfff46261efcdb847d55f3a7
atg-abhijay <atg.abhijay@gmail.com>
2018-12-23 18:35:09 -0500

    198: House Robber - initial commit

---
+++ HouseRobber198.java
@@ -0,0 +1,18 @@
+/**
+ * URL of problem:
+ * https://leetcode.com/problems/house-robber/
+ */
+
+public class HouseRobber198 {
+    public static void main(String[] args) {
+        Solution198 sol = new Solution198();
+        int[] nums = {};
+        System.out.println(sol.rob(nums));
+    }
+}
+
+class Solution198 {
+    public int rob(int[] nums) {
+        return -1;
+    }
+}

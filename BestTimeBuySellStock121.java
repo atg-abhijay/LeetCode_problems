a2676b2fbaa70c5e491f174909c48339e22c4290
atg-abhijay <atg.abhijay@gmail.com>
2018-12-23 19:08:18 -0500

    121: Best Time to Buy and Sell Stock - initial commit

---
+++ BestTimeBuySellStock121.java
@@ -0,0 +1,18 @@
+/**
+ * URL of problem:
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+ */
+
+public class BestTimeBuySellStock121 {
+    public static void main(String[] args) {
+        Solution121 sol = new Solution121();
+        int[] prices = {};
+        System.out.println(sol.maxProfit(prices));
+    }
+}
+
+class Solution121 {
+    public int maxProfit(int[] prices) {
+        return -1;
+    }
+}

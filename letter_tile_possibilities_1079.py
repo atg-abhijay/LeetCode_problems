dfdbff58bf20d3a3519206229c8740ce7238289a
Abhijay Gupta <atg.abhijay@gmail.com>
2019-06-24 12:14:00 -0400

    1079: Letter Tile Possibilities - initial commit

---
+++ letter_tile_possibilities_1079.py
@@ -0,0 +1,13 @@
+"""
+URL of problem:
+https://leetcode.com/problems/letter-tile-possibilities/
+"""
+
+
+class Solution(object):
+    def numTilePossibilities(self, tiles):
+        """
+        :type tiles: str
+        :rtype: int
+        """
+        return -1

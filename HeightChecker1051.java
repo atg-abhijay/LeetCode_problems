b4bda49b2e911fee566df75cac1277973ee5ecaa
Abhijay Gupta <atg.abhijay@gmail.com>
2019-06-24 01:31:58 -0400

    1051: Height Checker - initial commit

---
+++ HeightChecker1051.java
@@ -0,0 +1,18 @@
+/**
+ * URL of problem:
+ * https://leetcode.com/problems/height-checker/
+ */
+
+public class HeightChecker1051 {
+    public static void main(String[] args){
+        Solution1051 sol = new Solution1051();
+        int[] heights = {};
+        System.out.println(sol.heightChecker(heights));
+    }
+}
+
+class Solution1051 {
+    public int heightChecker(int[] heights) {
+        return -1;
+    }
+}

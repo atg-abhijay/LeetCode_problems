5412c43656ea63f15b1127a7c79a9cde31dd554f
Abhijay Gupta <atg.abhijay@gmail.com>
2019-06-24 01:40:16 -0400

    solved 1051: Height Checker

--- HeightChecker1051.java
+++ HeightChecker1051.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * URL of problem:
  * https://leetcode.com/problems/height-checker/
@@ -6,13 +8,22 @@
 public class HeightChecker1051 {
     public static void main(String[] args){
         Solution1051 sol = new Solution1051();
-        int[] heights = {};
+        int[] heights = {1,1,4,2,1,3};
         System.out.println(sol.heightChecker(heights));
     }
 }
 
 class Solution1051 {
     public int heightChecker(int[] heights) {
-        return -1;
+        int numElements = heights.length;
+        int[] copyOFHeights = Arrays.copyOf(heights, numElements);
+        Arrays.sort(copyOFHeights);
+        int numOutOfPlace = 0;
+        for(int i = 0; i < numElements; i++) {
+            if(heights[i] != copyOFHeights[i]) {
+                numOutOfPlace++;
+            }
+        }
+        return numOutOfPlace;
     }
 }

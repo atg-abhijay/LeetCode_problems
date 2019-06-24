a313713a7e9b6a8a0b348ae926331cd419665bbd
Abhijay Gupta <atg.abhijay@gmail.com>
2019-06-24 01:19:14 -0400

    solved 977: Squares of a Sorted Array

--- SquaresOfSortedArray977.java
+++ SquaresOfSortedArray977.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * URL of problem:
  * https://leetcode.com/problems/squares-of-a-sorted-array/
@@ -6,16 +8,23 @@
 public class SquaresOfSortedArray977 {
     public static void main(String[] args) {
         Solution977 sol = new Solution977();
-        int[] A = {};
+        int[] A = {-7,-3,2,3,11};
         int[] squares = sol.sortedSquares(A);
         for(int i = 0; i < squares.length; i++) {
-            System.out.println(squares[i] + " ");
+            System.out.print(squares[i] + " ");
         }
+        System.out.println();
     }
 }
 
 class Solution977 {
     public int[] sortedSquares(int[] A) {
-        return new int[] {-1};
+        int numElements = A.length;
+        int[] squares = new int[numElements];
+        for(int i = 0; i < numElements; i++) {
+            squares[i] = A[i] * A[i];
+        }
+        Arrays.sort(squares);
+        return squares;
     }
 }

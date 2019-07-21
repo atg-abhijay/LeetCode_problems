3ca61f5b736a74f70d1ecd0f60c52f030a9adda1
Abhijay Gupta <atg.abhijay@gmail.com>
2019-07-21 18:47:04 -0400

    1008: Construct BST from Preorder Traversal - initial commit

---
+++ BSTfromPreorderTraversal1008.java
@@ -0,0 +1,24 @@
+/*
+ * URL of problem:
+ * https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/
+ */
+
+// Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+public class BSTfromPreorderTraversal1008 {
+    public static void main(String[] args) {
+        return;
+    }
+}
+
+class Solution {
+    public TreeNode bstFromPreorder(int[] preorder) {
+        return new TreeNode(-1);
+    }
+}

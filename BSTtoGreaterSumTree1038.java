0309eb3b848c98b5c2e964c8d5931aca2de30f81
Abhijay Gupta <atg.abhijay@gmail.com>
2019-07-21 17:19:08 -0400

    1038: BST to Greater Sum Tree - initial commit

---
+++ BSTtoGreaterSumTree1038.java
@@ -0,0 +1,24 @@
+/*
+ * URL of problem:
+ * https://leetcode.com/problems/binary-search-tree-to-greater-sum-tree/
+ */
+
+// Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+public class BSTtoGreaterSumTree1038 {
+    public static void main(String[] args) {
+        return;
+    }
+}
+
+class Solution1038 {
+    public TreeNode bstToGst(TreeNode root) {
+        return new TreeNode(-1);
+    }
+}

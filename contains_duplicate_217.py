a2127a2aba696445d3216fc4a565eb2e0936eb8d
Abhijay Gupta <atg.abhijay@gmail.com>
2019-09-16 13:34:40 -0400

    217: Contains Duplicate - initial commit

---
+++ contains_duplicate_217.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/contains-duplicate/
+"""
+
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        pass
+
+
+def main():
+    print(Solution().containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]))
+
+
+if __name__ == '__main__':
+    main()

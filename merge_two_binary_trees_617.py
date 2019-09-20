b713b3b3897fc8c558b13d2ceef331ce271e26c2
Abhijay Gupta <atg.abhijay@gmail.com>
2019-09-20 00:07:47 -0400

    617: Merge Two Binary Trees - initial commit

---
+++ merge_two_binary_trees_617.py
@@ -0,0 +1,17 @@
+"""
+URL of problem:
+https://leetcode.com/problems/merge-two-binary-trees/
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
+        return None

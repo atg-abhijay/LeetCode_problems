af47f5dacba75e4c67524c1b1ad3231e57a08de7
Abhijay Gupta <atg.abhijay@gmail.com>
2019-09-19 23:52:06 -0400

    Add comments and update variable name

--- max_level_sum_binary_tree_1161.py
+++ max_level_sum_binary_tree_1161.py
@@ -18,16 +18,19 @@ class TreeNode:
 class Solution:
     def maxLevelSum(self, root: TreeNode) -> int:
         lvl_sums = [-inf]
-        max_sum, max_sum_index = lvl_sums[0], 0
+        max_sum, max_sum_lvl = lvl_sums[0], 0
         queue: List[TreeNode] = [root]
         root.lvl = 1
         while queue:
             node: TreeNode = queue.pop(0)
             if node.lvl == len(lvl_sums):
                 lvl_sums.append(node.val)
+                # At this stage, the sum for the previous level
+                # is complete. Compare it with the current max
+                # sum and check if the max sum needs to be updated.
                 if lvl_sums[-2] > max_sum:
                     max_sum = lvl_sums[-2]
-                    max_sum_index = len(lvl_sums) - 2
+                    max_sum_lvl = len(lvl_sums) - 2
             else:
                 lvl_sums[node.lvl] += node.val
 
@@ -36,4 +39,4 @@ class Solution:
                     child.lvl = node.lvl + 1
                     queue.append(child)
 
-        return max_sum_index
+        return max_sum_lvl

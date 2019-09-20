3d52b55b521ccc26490e81e95c034928e0797fd6
Abhijay Gupta <atg.abhijay@gmail.com>
2019-09-19 23:48:45 -0400

    Make a slight optimization
    Keep track of the max sum and the level at which it occurs
    whilst iterating instead of calculating them at the end.

--- max_level_sum_binary_tree_1161.py
+++ max_level_sum_binary_tree_1161.py
@@ -18,12 +18,16 @@ class TreeNode:
 class Solution:
     def maxLevelSum(self, root: TreeNode) -> int:
         lvl_sums = [-inf]
+        max_sum, max_sum_index = lvl_sums[0], 0
         queue: List[TreeNode] = [root]
         root.lvl = 1
         while queue:
             node: TreeNode = queue.pop(0)
             if node.lvl == len(lvl_sums):
                 lvl_sums.append(node.val)
+                if lvl_sums[-2] > max_sum:
+                    max_sum = lvl_sums[-2]
+                    max_sum_index = len(lvl_sums) - 2
             else:
                 lvl_sums[node.lvl] += node.val
 
@@ -32,4 +36,4 @@ class Solution:
                     child.lvl = node.lvl + 1
                     queue.append(child)
 
-        return lvl_sums.index(max(lvl_sums))
+        return max_sum_index

9774e96c2250fe220b39775d771ca0fbd028f392
Abhijay Gupta <atg.abhijay@gmail.com>
2019-09-19 23:08:18 -0400

    1161: Maximum Level Sum of a Binary Tree - initial commit

---
+++ max_level_sum_binary_tree_1161.py
@@ -0,0 +1,17 @@
+"""
+URL of problem:
+https://leetcode.com/problems/maximum-level-sum-of-a-binary-tree/
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def maxLevelSum(self, root: TreeNode) -> int:
+        pass

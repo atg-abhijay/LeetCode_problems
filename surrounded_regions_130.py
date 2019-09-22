61e61c648570b9d6387de9a4df9ef6fcc1cffe45
Abhijay Gupta <atg.abhijay@gmail.com>
2019-09-22 00:27:45 -0400

    130: Surrounded Regions - initial commit

---
+++ surrounded_regions_130.py
@@ -0,0 +1,21 @@
+"""
+URL of problem:
+https://leetcode.com/problems/surrounded-regions/
+"""
+
+
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        pass
+
+
+def main():
+    Solution().solve([["X", "X", "X", "X"], ["X", "O", "O", "X"], [
+        "X", "X", "O", "X"], ["X", "O", "X", "X"]])
+
+
+if __name__ == '__main__':
+    main()

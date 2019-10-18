1fb7a51783e3666e070f01e9d2b0e8daa7db38ef
Abhijay Gupta <atg.abhijay@gmail.com>
2019-10-18 12:06:00 -0400

    solved 841: Keys and Rooms

--- keys_and_rooms_841.py
+++ keys_and_rooms_841.py
@@ -4,13 +4,36 @@ https://leetcode.com/problems/keys-and-rooms/
 """
 
 
+class Room:
+    def __init__(self, room_number, neighbours):
+        self.room_number = room_number
+        self.neighbour_indexes = neighbours
+        self.visited = False
+
+
 class Solution(object):
     def canVisitAllRooms(self, rooms):
         """
         :type rooms: List[List[int]]
         :rtype: bool
         """
-        pass
+        room_nodes = []
+        for index, neighbours in enumerate(rooms):
+            room_nodes.append(Room(index, neighbours))
+
+        queue = [room_nodes[0]]
+        while queue:
+            room = queue.pop(0)
+            if room.visited:
+                continue
+
+            room.visited = True
+            for nb_index in room.neighbour_indexes:
+                nb = room_nodes[nb_index]
+                if not nb.visited:
+                    queue.append(nb)
+
+        return all(room.visited for room in room_nodes)
 
 
 def main():

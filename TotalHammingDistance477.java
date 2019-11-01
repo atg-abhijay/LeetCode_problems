9a8d57fea84ccb5c57685965f603d8a9dd0523f2
Abhijay Gupta <atg.abhijay@gmail.com>
2019-11-01 12:13:09 -0400

    477: Total Hamming Distance - initial commit

---
+++ TotalHammingDistance477.java
@@ -0,0 +1,18 @@
+/**
+ * URL of problem:
+ * https://leetcode.com/problems/total-hamming-distance/
+ */
+
+public class TotalHammingDistance477 {
+    public static void main(String[] args) {
+        Solution477 sol = new Solution477();
+        int[] nums = new int[]{4, 14, 2};
+        System.out.println(sol.totalHammingDistance(nums));
+    }
+}
+
+class Solution477 {
+    public int totalHammingDistance(int[] nums) {
+        return -1;
+    }
+}

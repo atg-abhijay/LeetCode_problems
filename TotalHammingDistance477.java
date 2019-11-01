3acd8b47e85d448044c4735bd74b89a8fd7eb6d0
Abhijay Gupta <atg.abhijay@gmail.com>
2019-11-01 12:18:52 -0400

    Time limit exceeded

--- TotalHammingDistance477.java
+++ TotalHammingDistance477.java
@@ -13,6 +13,16 @@ public class TotalHammingDistance477 {
 
 class Solution477 {
     public int totalHammingDistance(int[] nums) {
-        return -1;
+        int hammingDistance = 0;
+        for(int i = 0; i < nums.length-1; i++) {
+            for(int j = i; j < nums.length; j++) {
+                int xorResult = nums[i] ^ nums[j];
+                while(xorResult > 0) {
+                    hammingDistance += xorResult & 1;
+                    xorResult = xorResult >> 1;
+                }
+            }
+        }
+        return hammingDistance;
     }
 }

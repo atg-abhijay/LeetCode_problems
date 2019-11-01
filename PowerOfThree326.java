c061affa452d4f9f2e561ae0fdeb678cd671a9b5
Abhijay Gupta <atg.abhijay@gmail.com>
2019-10-31 23:57:31 -0400

    326: Power of Three - initial commit

---
+++ PowerOfThree326.java
@@ -0,0 +1,17 @@
+/**
+ * URL of problem:
+ * https://leetcode.com/problems/power-of-three/
+ */
+
+public class PowerOfThree326 {
+    public static void main(String[] args) {
+        Solution326 sol = new Solution326();
+        System.out.println(sol.isPowerOfThree(45));
+    }
+}
+
+class Solution326 {
+    public boolean isPowerOfThree(int n) {
+        return false;
+    }
+}

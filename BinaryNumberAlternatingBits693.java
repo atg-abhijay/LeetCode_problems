66f1a064bdee948ebfcd51825f7f72850cbd040a
Abhijay Gupta <atg.abhijay@gmail.com>
2019-11-01 11:51:38 -0400

    693: Binary Number with Alternating Bits - initial commit

---
+++ BinaryNumberAlternatingBits693.java
@@ -0,0 +1,17 @@
+/**
+ * URL of problem:
+ * https://leetcode.com/problems/binary-number-with-alternating-bits/
+ */
+
+public class BinaryNumberAlternatingBits693 {
+    public static void main(String[] args) {
+        Solution693 sol = new Solution693();
+        System.out.println(sol.hasAlternatingBits(11));
+    }
+}
+
+class Solution693 {
+    public boolean hasAlternatingBits(int n) {
+        return false;
+    }
+}

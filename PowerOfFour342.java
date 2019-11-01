6515c0ac8995550610c8826332e14d3e929995d1
Abhijay Gupta <atg.abhijay@gmail.com>
2019-10-31 23:48:30 -0400

    342: Power of Four - initial commit

---
+++ PowerOfFour342.java
@@ -0,0 +1,17 @@
+/**
+ * URL of problem:
+ * https://leetcode.com/problems/power-of-four/
+ */
+
+public class PowerOfFour342 {
+    public static void main(String[] args) {
+        Solution342 sol = new Solution342();
+        System.out.println(sol.isPowerOfFour(5));
+    }
+}
+
+class Solution342 {
+    public boolean isPowerOfFour(int num) {
+        return false;
+    }
+}

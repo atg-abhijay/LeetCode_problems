b3438aaed28139b8f700a1ed6f932ba59a46a5c1
Abhijay Gupta <atg.abhijay@gmail.com>
2019-11-01 11:27:18 -0400

    Working for positive numbers

--- NumberOf1Bits191.java
+++ NumberOf1Bits191.java
@@ -6,13 +6,26 @@
 public class NumberOf1Bits191 {
     public static void main(String[] args) {
         Solution191 sol = new Solution191();
-        System.out.println(sol.hammingWeight(56));
+        System.out.println(sol.hammingWeight(0b11111111111111111111111111111101));
     }
 }
 
 class Solution191 {
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
-        return -1;
+        n = Math.abs(n);
+        int[] num1Bits = new int[n+1];
+        boolean odd = true;
+        for(int i = 1; i <= n; i++) {
+            if(odd) {
+                num1Bits[i] = num1Bits[i-1] + 1;
+                odd = false;
+            }
+            else {
+                num1Bits[i] = num1Bits[i/2];
+                odd = true;
+            }
+        }
+        return num1Bits[n];
     }
 }

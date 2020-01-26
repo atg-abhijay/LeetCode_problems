85f739ff8ff51c93ca61bf1a15c7bce4da79e061
Abhijay Gupta <atg.abhijay@gmail.com>
2020-01-25 23:45:48 -0500

    111: Minimum Depth of Binary Tree - initial commit

---
+++ min_depth_binary_tree_111.py
@@ -0,0 +1,17 @@
+"""
+URL of problem:
+https://leetcode.com/problems/minimum-depth-of-binary-tree/
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        return -1

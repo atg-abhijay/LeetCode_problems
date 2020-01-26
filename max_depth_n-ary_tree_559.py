0faa61392c62f287cf22dbc08bbe7b94ddeeccbf
Abhijay Gupta <atg.abhijay@gmail.com>
2020-01-25 23:38:22 -0500

    559: Maximum Depth of N-ary Tree - initial commit

---
+++ max_depth_n-ary_tree_559.py
@@ -0,0 +1,16 @@
+"""
+URL of problem:
+https://leetcode.com/problems/maximum-depth-of-n-ary-tree/
+"""
+
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        return -1

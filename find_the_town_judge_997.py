6487685c98126d7aa1314688dbf39a9598e57188
Abhijay Gupta <atg.abhijay@gmail.com>
2020-01-26 00:22:03 -0500

    solved 997: Find the Town Judge

--- find_the_town_judge_997.py
+++ find_the_town_judge_997.py
@@ -6,4 +6,16 @@ https://leetcode.com/problems/find-the-town-judge/
 
 class Solution:
     def findJudge(self, N: int, trust: List[List[int]]) -> int:
+        if not trust:
+            return 1
+
+        trust_scores = [0 for x in range(N+1)]
+        for pair in trust:
+            trust_scores[pair[0]] -= 1
+            trust_scores[pair[1]] += 1
+
+        for index, score in enumerate(trust_scores):
+            if score == N-1:
+                return index
+
         return -1

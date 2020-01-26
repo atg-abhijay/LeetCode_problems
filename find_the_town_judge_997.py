e0ba0cb34fff5abf31200beb667b9d434eb64ea2
Abhijay Gupta <atg.abhijay@gmail.com>
2020-01-26 00:27:44 -0500

    997: added comments

--- find_the_town_judge_997.py
+++ find_the_town_judge_997.py
@@ -9,7 +9,11 @@ class Solution:
         if not trust:
             return 1
 
+        # The first value is a dummy
+        # value since N starts from 1
         trust_scores = [0 for x in range(N+1)]
+        # The Judge will be someone with a score of N-1
+        # since they trust no one and are trusted by everyone
         for pair in trust:
             trust_scores[pair[0]] -= 1
             trust_scores[pair[1]] += 1

5a9f20c33b68c5ae0039548ee52fc27e1ec0e7b9
Abhijay Gupta <atg.abhijay@gmail.com>
2020-07-23 18:52:27 -0400

    191 - Number of 1 Bits (#16)
    * 191: Number of 1 Bits - initial commit
    
    * Working for positive numbers
    
    * solve 191: Number of 1 Bits
    
    * 191: add comments

---
+++ NumberOf1Bits191.java
@@ -0,0 +1,28 @@
+/**
+ * URL of problem:
+ * https://leetcode.com/problems/number-of-1-bits/
+ */
+
+public class NumberOf1Bits191 {
+    public static void main(String[] args) {
+        Solution191 sol = new Solution191();
+        System.out.println(sol.hammingWeight(0b11111111111111111111111111111101));
+    }
+}
+
+class Solution191 {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        /**
+         * use unsigned right shift operator.
+         * repeatedly extract rightmost bit
+         * until n becomes 0.
+         */
+        int num1Bits = 0;
+        while(n != 0) {
+            num1Bits += (n & 1);
+            n = n >>> 1;
+        }
+        return num1Bits;
+    }
+}

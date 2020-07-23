31a225e9cb8a9add337c83ee7b7f4dc1b464c45b
Abhijay Gupta <atg.abhijay@gmail.com>
2020-07-23 18:45:16 -0400

    solve 191: Number of 1 Bits

--- NumberOf1Bits191.java
+++ NumberOf1Bits191.java
@@ -13,19 +13,11 @@ public class NumberOf1Bits191 {
 class Solution191 {
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
-        n = Math.abs(n);
-        int[] num1Bits = new int[n+1];
-        boolean odd = true;
-        for(int i = 1; i <= n; i++) {
-            if(odd) {
-                num1Bits[i] = num1Bits[i-1] + 1;
-                odd = false;
-            }
-            else {
-                num1Bits[i] = num1Bits[i/2];
-                odd = true;
-            }
+        int num1Bits = 0;
+        while(n != 0) {
+            num1Bits += (n & 1);
+            n = n >>> 1;
         }
-        return num1Bits[n];
+        return num1Bits;
     }
 }

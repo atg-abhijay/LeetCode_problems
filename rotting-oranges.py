81dff315cf8736ae644364293cebe67950ae1eda
Abhijay Gupta <atg.abhijay@gmail.com>
2020-08-10 23:55:21 -0400

    Add comments and update README

--- rotting-oranges.py
+++ rotting-oranges.py
@@ -1,6 +1,7 @@
 """
 URL of problem:
 https://leetcode.com/explore/challenge/card/august-leetcoding-challenge/550/week-2-august-8th-august-14th/3418/
+https://leetcode.com/problems/rotting-oranges/
 """
 
 
@@ -10,7 +11,7 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
-        # surround existing grid with empty cells
+        # Surround existing grid with empty cells
         # to avoid having to handle grid edge cases
         num_rows = len(grid)
         num_cols = len(grid[0])
@@ -27,11 +28,17 @@ class Solution(object):
                 if grid[i][j] == 2:
                     rotten_idxs.append([i, j])
 
+        # Run a BFS from EACH rotten orange. Each
+        # time a level is explored AND at least
+        # one orange is made rotten as a result
+        # overall, increment the minutes elapsed.
         minutes_elapsed = 0
         should_stop = False
         while not should_stop:
             new_idxs = []
             did_spoil_this_turn = False
+            # The entire list of indexes is
+            # explored within the same 1 minute.
             for position in rotten_idxs:
                 tmp_result = self.rottenTheNeighbours(grid, position, new_idxs)
                 did_spoil_this_turn = did_spoil_this_turn or tmp_result
@@ -43,9 +50,9 @@ class Solution(object):
             else:
                 should_stop = True
 
-        # if there are any fresh oranges left
+        # If there are any fresh oranges left
         # at the end, then it is impossible
-        # to rotten all of them
+        # to rotten all of them.
         for i in range(1, num_rows + 1):
             for j in range(1, num_cols + 1):
                 if grid[i][j] == 1:
@@ -57,6 +64,7 @@ class Solution(object):
         row_idx = position[0]
         col_idx = position[1]
         did_spoil_this_turn = False
+
         # West
         if grid[row_idx][col_idx-1] == 1:
             grid[row_idx][col_idx-1] = 2

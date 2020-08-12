c4e165e89653dcd8a016edf2f2fb43688cc7834e
Abhijay Gupta <atg.abhijay@gmail.com>
2020-08-11 23:50:10 -0400

    Excel Sheet Column Number - initial commit

---
+++ excel-sheet-column-number.py
@@ -0,0 +1,14 @@
+"""
+URL of problem:
+https://leetcode.com/explore/challenge/card/august-leetcoding-challenge/550/week-2-august-8th-august-14th/3419/
+https://leetcode.com/problems/excel-sheet-column-number/
+"""
+
+
+class Solution(object):
+    def titleToNumber(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        return -1

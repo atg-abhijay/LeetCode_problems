980dad8987fe64bad89aae5616519b20f2fe2d6a
Abhijay Gupta <atg.abhijay@gmail.com>
2020-08-16 19:26:17 -0400

    122: Best Time to Buy and Sell Stock II - initial commit

---
+++ best_time_buy_sell_stock_ii_122.py
@@ -0,0 +1,13 @@
+"""
+URL of problem:
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+"""
+
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        return -1

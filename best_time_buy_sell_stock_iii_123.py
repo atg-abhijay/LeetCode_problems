44a52d7222d6aa81d2fd1166aa8f4b848369e0f8
Abhijay Gupta <atg.abhijay@gmail.com>
2020-08-16 19:08:24 -0400

    123: Best Time to Buy and Sell Stock III - 190/200 test cases pass

--- best_time_buy_sell_stock_iii_123.py
+++ best_time_buy_sell_stock_iii_123.py
@@ -11,4 +11,51 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        return -1
+        if not prices:
+            return 0
+
+        trsac_profit = 0
+        transaction_profits = []
+        is_tp_appended = False
+        purchase_price = prices[0]
+        for idx, selling_price in enumerate(prices):
+            if selling_price < purchase_price or (idx > 0 and selling_price < prices[idx-1]):
+                purchase_price = selling_price
+                transaction_profits.append(trsac_profit)
+                is_tp_appended = True
+                trsac_profit = 0
+            else:
+                if selling_price - purchase_price > trsac_profit:
+                    trsac_profit = selling_price - purchase_price
+
+                is_tp_appended = False
+
+        if not is_tp_appended:
+            transaction_profits.append(trsac_profit)
+
+        max_profit = self.findAndPopMax(transaction_profits)
+        max_profit += self.findAndPopMax(transaction_profits)
+
+        return max_profit
+
+    def findAndPopMax(self, lst):
+        if not lst:
+            return 0
+
+        max_val = 0
+        idx_of_max = 0
+        for idx, elem in enumerate(lst):
+            if elem > max_val:
+                max_val = elem
+                idx_of_max = idx
+
+        lst.pop(idx_of_max)
+        return max_val
+
+
+def main():
+    print(Solution().maxProfit([1, 2, 4, 2, 5, 7, 2, 4, 9, 0]))
+
+
+if __name__ == '__main__':
+    main()

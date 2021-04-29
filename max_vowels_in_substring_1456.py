b08e20cb3674462f29ff9ebbadbad840059b2837
Abhijay Gupta <atg.abhijay@gmail.com>
2021-04-28 21:02:34 -0400

    Initial commit

---
+++ max_vowels_in_substring_1456.py
@@ -0,0 +1,14 @@
+"""
+URL of problem:
+https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length/
+"""
+
+
+class Solution(object):
+    def maxVowels(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        return -1

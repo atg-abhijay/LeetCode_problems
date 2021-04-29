cdd545de80d0badb111d06eb8ab10c60cff45b99
Abhijay Gupta <atg.abhijay@gmail.com>
2021-04-28 23:01:06 -0400

    Initial commit

---
+++ reducing_dishes_1402.py
@@ -0,0 +1,13 @@
+"""
+URL of problem:
+https://leetcode.com/problems/reducing-dishes/
+"""
+
+
+class Solution(object):
+    def maxSatisfaction(self, satisfaction):
+        """
+        :type satisfaction: List[int]
+        :rtype: int
+        """
+        return -1
